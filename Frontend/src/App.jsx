e39594c772c7274ccf3505bf11b322d944e6d646
mohammadruman <mohammaderuman@gmail.com>
2024-09-01 16:11:37 +0530

    fix: custom navbar for login and register page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,23 +5,15 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './pages/Login'
 import Register from './pages/Register'
-
-
-
-
-const App = () => {
-
-
-  
+const App = () => {  
   return (
      <div>
-        <Navbar/>
       <Routes>
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Register/>}/>
       </Routes>
-    <Footer/>
+  
     </div>
   )
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Homepost from '../components/Homepost';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Home = () => {
+
   return (
-    <div className='className="px-8 md:px-[200px] min-h-[80vh]'>
+    <>
+    <Navbar/>
+    <div className="px-8 md:px-[200px]">
         <Homepost/>
         <Homepost/>
         <Homepost/>
         <Homepost/>
-        <Homepost/>
-        
+        <Homepost/>  
     </div>
+    <Footer/>
+    </>
   )
 }
 
