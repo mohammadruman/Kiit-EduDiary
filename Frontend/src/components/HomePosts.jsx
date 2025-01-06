bc0bf475d2102cbc62085f7314ff61f9494acbb2
mohammadruman <mohammaderuman@gmail.com>
2025-01-07 00:25:20 +0530

    update : UI of the home

--- Frontend/src/components/HomePosts.jsx
+++ Frontend/src/components/HomePosts.jsx
@@ -1,32 +1,35 @@
 /* eslint-disable react/prop-types */
-import {IF} from '../url'
+import { IF } from "../url";
 
-
-const HomePosts = ({post}) => {
+const HomePosts = ({ post }) => {
   return (
-    <div className="w-full flex mt-8 space-x-4">
-    {/* left */}
-    <div className="w-[35%] h-[200px] flex justify-center items-center">
-    <img src={IF+post.photo} alt="" className="h-full w-full object-cover"/>
-    </div>
-    {/* right */}
-    <div className="flex flex-col w-[65%]">
-      <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
-      {post.title}
-      </h1>
-      <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4">
-       <p>@{post.username}</p>
-       <div className="flex space-x-2 text-sm">
-       <p>{new Date(post.updatedAt).toString().slice(0,15)}</p>
-       <p>{new Date(post.updatedAt).toString().slice(16,24)}</p>
-       </div>
+    <div className="w-full flex flex-col md:flex-row md:space-x-4 mt-8 p-4 rounded-lg border hover:shadow-lg hover:scale-[1.02] transition-transform duration-300 ease-in-out ">
+      {/* Left */}
+      {post.photo && (
+        <div className="w-full md:w-[35%] h-[200px] flex justify-center items-center mb-4 md:mb-0">
+          <img
+            src={IF + post.photo}
+            alt=""
+            className="h-full w-full object-cover rounded-lg"
+          />
+        </div>
+      )}
+      {/* Right */}
+      <div className="flex flex-col w-full md:w-[100%]">
+        <h1 className="text-lg md:text-2xl font-bold mb-2">{post.title}</h1>
+        <div className="flex justify-between items-center text-sm text-gray-500 mb-2">
+          <p>@{post.username}</p>
+          <div className="flex space-x-2">
+            <p>{new Date(post.updatedAt).toDateString()}</p>
+            <p>{new Date(post.updatedAt).toLocaleTimeString()}</p>
+          </div>
+        </div>
+        <p className="text-sm ">
+          {post.desc.slice(0, 400) + " ...Read more"}
+        </p>
       </div>
-      <p className="text-sm md:text-lg">{post.desc.slice(0,200)+" ...Read more"}</p>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default HomePosts
-
+export default HomePosts;
