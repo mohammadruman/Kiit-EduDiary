7f00f2a9486d3e77b6aec3df84d1a8a045a88d75
mohammadruman <mohammaderuman@gmail.com>
2025-01-06 19:07:09 +0530

    fix bug

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
          <p>Terms of Service</p>
        </div>
     </div>
-    <p className="py-2 pb-6 text-center text-white bg-black text-sm">All rights reserved @Blog Market 2023</p>
+    <p className="py-2 pb-6 text-center text-white bg-black text-sm">All rights reserved @AlgoNarratives 2024</p>
     </>
     
   )
