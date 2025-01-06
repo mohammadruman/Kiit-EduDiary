cf4f699b7f98daec8d667f84ad3a929e479a331d
mohammadruman <mohammaderuman@gmail.com>
2025-01-06 18:57:27 +0530

    fix: cors error in the app

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -24,29 +24,57 @@ router.post("/register",async(req,res)=>{
 
 
 //LOGIN
-router.post("/login",async (req,res)=>{
-    try{
-        const user=await User.findOne({email:req.body.email})
+// router.post("/login",async (req,res)=>{
+//     try{
+//         const user=await User.findOne({email:req.body.email})
        
-        if(!user){
-            return res.status(404).json("User not found!")
-        }
-        const match=await bcrypt.compare(req.body.password,user.password)
+//         if(!user){
+//             return res.status(404).json("User not found!")
+//         }
+//         const match=await bcrypt.compare(req.body.password,user.password)
         
-        if(!match){
-            return res.status(401).json("Wrong credentials!")
+//         if(!match){
+//             return res.status(401).json("Wrong credentials!")
+//         }
+//         res.status(200).json(user)
+//         const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})
+//         const {password,...info}=user._doc
+//         res.cookie("token",token).status(200).json(info)
+
+//     }
+//     catch(err){
+//         res.status(500).json(err)
+//     }
+// })
+
+//New login route for testing
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json("User not found!");
         }
-        res.status(200).json(user)
-        const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})
-        const {password,...info}=user._doc
-        res.cookie("token",token).status(200).json(info)
 
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+        const match = await bcrypt.compare(req.body.password, user.password);
 
+        if (!match) {
+            return res.status(401).json("Wrong credentials!");
+        }
+
+        const token = jwt.sign({ _id: user._id, username: user.username, email: user.email }, process.env.SECRET, { expiresIn: "3d" });
+        const { password, ...info } = user._doc;
+
+        // Set the token cookie and send the response with the user info
+        res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
+            .status(200)
+            .json(info); // Send the user data without password
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 //LOGOUT
