e9a9443a49422a30b3c4501ff5df31423ef3667b
Mohammad Ruman <88760648+mohammadruman@users.noreply.github.com>
2025-01-21 04:37:18 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom"
 import Footer from "../components/Footer"
 import { useContext, useState } from "react"
 import axios from "axios"
-import { URL } from "../url"
+
 import { UserContext } from "../context/UserContext"
 
 
